53985424f40c83ac1d4013617ba5039e78da4b98
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-06-05 11:56:25 +0200

    hva er 0.1.1

--- src/components/layout/superfan/superfan.tsx
+++ src/components/layout/superfan/superfan.tsx
@@ -2,31 +2,82 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+
 export function Superfan() {
     return (
-        <section className='w-full flex flex-col md:flex-row'>
-            <Image
-                className="rounded-full w-80 h-80 object-cover object-center self-center mt-10 "
-                src="/Superfan.jpg"
-                alt="Superfan"
-                width={200}
-                height={200}
-                draggable={false}
-            />
+        <section className='flex flex-col items-center w-full md:flex-row md:justify-center '>           
+                    <Image
+                    className="fade-edge md:w-1/3"
+                    src="/Superfan.jpg"
+                    alt="Superfan"
+                    width={320}
+                    height={320}
+                    draggable={false}
+                     />                
 
-            <div className='md:w-1/2 flex flex-col'>
-                <h1 className='text-6xl mt-10'>Hva er Superfan?</h1>
-                <p className='text-lg mt-6'>The Superfan app powered by Oiid allows the artist to create a curated safe space, where the artist and their most dedicated fans meet.</p>
-                <div className='mt-6'>
-                    <div>
-                        <h3 className='font-bold'>Våre fans og artister:</h3>
+                <div className='md:w-1/2 flex flex-col self-center md:ml-10'>
+                    <h1 className='text-6xl  md:ml-10'>Hva er Superfan?</h1>
+                    <p className='text-md mt-6 md:ml-11'>The Superfan app powered by Oiid allows the artist to create a curated safe space, where the artist and their most dedicated fans meet.</p>
+                        <div className='mt-6 md:ml-11'>
+                            <div>
+                             <h3 className='font-bold'>Våre fans og artister:</h3>
+                            </div>
+                            <div>
+                                <div className='flex flex-row'>
+                                <Image
+                                    className='border-3 p-1 border-indigo-600 rounded-full'
+                                    src="/Rectangle 19.png"
+                                    alt=""
+                                    width={320}
+                                    height={320}
+                                    draggable={false}
+                                    />
+                                    <Image
+                                    className='border-3 p-1 border-indigo-600 rounded-full'
+                                    src="/Rectangle 20.png"
+                                    alt=""
+                                    width={320}
+                                    height={320}
+                                    draggable={false}
+                                    />
+                                    <Image
+                                    className='border-3 p-1 border-indigo-600 rounded-full'
+                                    src="/Rectangle 21.png"
+                                    alt=""
+                                    width={320}
+                                    height={320}
+                                    draggable={false}
+                                    />
+                                    <Image
+                                    className='border-3 p-1 border-indigo-600 rounded-full'
+                                    src="/Rectangle 22.png"
+                                    alt=""
+                                    width={320}
+                                    height={320}
+                                    draggable={false}
+                                    />
+                                    <Image
+                                    className='border-3 p-1 border-indigo-600 rounded-full'
+                                    src="/Rectangle 23.png"
+                                    alt=""
+                                    width={320}
+                                    height={320}
+                                    draggable={false}
+                                    />
+                                    <Image
+                                    className='border-3 p-1 border-indigo-600 rounded-full'
+                                    src="/Rectangle 24.png"
+                                    alt=""
+                                    width={320}
+                                    height={320}
+                                    draggable={false}
+                                    />
+                            </div>
+                            <p>komentar felt</p>
+                            </div>
+                            
                     </div>
-                    <div>
-                        <p>Karusell (med komentar)</p>
-                    </div>
-                </div>
-            </div>
+                </div>                        
         </section>
-
     );
 }
