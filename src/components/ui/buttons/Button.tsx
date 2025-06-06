64863915d66091b76002a04a527c77175b190121
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-06-06 12:01:54 +0200

    added button comp. added to how.tsx. added tw

---
+++ src/components/ui/buttons/Button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+type ButtonProps = {
+  children?: React.ReactNode;
+
+  href?: string;
+  className?: string;
+
+  onClick?: () => void;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({
+  children,
+  href,
+  className,
+
+  onClick,
+}: ButtonProps) {
+  const base = `
+          py-3 px-6  rounded-sm  bg-light-purple text-white font-semibold hover:bg-[#4E208E] cursor-pointer transition duration-300 disabled:opacity-60 disabled:cursor-not-allowed`;
+
+  const combined = `${base} ${className}`;
+
+  if (href) {
+    return (
+      <Link href={href} className={combined} tabIndex={0}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button onClick={onClick} className={combined}>
+      {children}
+    </button>
+  );
+}
