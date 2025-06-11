879c85856859c00c24ddc7aa4ca9fc2d1dbad21c
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-06-11 09:15:25 +0200

    button prop fix

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -4,21 +4,14 @@ import React from "react";
 import Link from "next/link";
 
 type ButtonProps = {
-  children?: React.ReactNode;
+  children: React.ReactNode;
 
   href?: string;
   className?: string;
-
   onClick?: () => void;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({
-  children,
-  href,
-  className,
-
-  onClick,
-}: ButtonProps) {
+export function Button({ children, href, className, onClick }: ButtonProps) {
   const base = `
           py-3 px-6  rounded-sm  bg-light-purple text-white font-semibold hover:bg-[#4E208E] cursor-pointer transition duration-300 disabled:opacity-60 disabled:cursor-not-allowed`;
 
