64a50e1f6b5fefe435e4d4d008115f0947ac2772
Monika <maam092024@gmail.com>
2025-06-24 14:39:09 +0200

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "@/assets/styles/globals.css";
 import { Header } from "@/components/layout/header/header";
-import Footer from "@/components/layout/footer/footer";
+import Footer from "../components/layout/footer/footer";
 import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
