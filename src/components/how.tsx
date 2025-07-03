4c5ec8520d4cfa0cb60eab778a41dac9de21b929
Monika <maam092024@gmail.com>
2025-07-03 11:09:01 +0200

    divs on mobile/md

--- src/components/how.tsx
+++ src/components/how.tsx
@@ -6,66 +6,118 @@ import Image from "next/image";
 export function How() {
     return (
         <section id='how' className='pt-10'>
-            <div className='h-full w-full flex justify-center gap-10 items-center flex-col lg:flex-row '>
+            <div className='h-full w-full flex justify-center gap-10 items-center flex-col lg:flex-row'>
                 <div className='w-full text-start p-2 space-y-10 mb-10'>
-                    <h1 className='text-5xl lg:text-5xl font-semibold leading-tight font-raleway '>
+                    <h1 className='text-5xl lg:text-5xl font-semibold leading-tight font-raleway'>
                         How does it work?
                     </h1>
-                    <div className='space-y-6'>
-                        <h2 className='text-3xl font-raleway font-semibold'>
-                            Unleash your inner artist and become part of the
-                            fan-driven experience.
-                        </h2>
-                        <p className='md:w-4/5 '>
-                            Superfans can register their own accounts to create
-                            personalized versions of selected songs, interact
-                            and collaborate with other superfans, and get early
-                            access to concert tickets, exclusive events,
-                            physical releases, and merch.
-                        </p>
-                    </div>
+                </div>
+            </div>
+
+            <div className="slide1 flex flex-col lg:flex-row justify-center gap-10 items-center">
+                <div className='space-y-6'>
+                    <h2 className='text-3xl font-raleway font-semibold'>
+                    Artists & Superfans — Building It Together
+                    </h2>
+                    <p className='md:w-4/5'>
+Superfans can shape their own profiles, interact directly with artists, and connect with like-minded fans across the world. Artists benefit from a community that not only listens, but actively engages, creating a deeper connection and a more meaningful fan experience.                    </p>
                 </div>
                 <div className='relative flex justify-center items-center md:w-1/2'>
                     <div
-                        className=' 
-    absolute transition-all duration-500 ease-out z-10
-    w-full md:w-1/2 h-[320px] sm:h-[350px] md:h-[300px] lg:h-[400px] xl:h-[550px] 
-    bottom-8 -left-12 sm:bottom-12 sm:-left-17 md:bottom-10 md:left-10  lg:bottom-10 lg:left-10 xl:bottom-6 xl:left-12'
+                        className='absolute transition-all duration-500 ease-out z-10 w-full md:w-1/2 h-[320px] sm:h-[350px] md:h-[300px] lg:h-[400px] xl:h-[550px] bottom-8 -left-12 sm:bottom-12 sm:-left-17 md:bottom-10 md:left-10 lg:bottom-10 lg:left-10 xl:bottom-6 xl:left-12'
                     >
                         <Image
                             src='/images/Stepper.png'
                             alt='androidphone'
                             fill
-                            className='object-contain '
+                            className='object-contain'
                         />
                     </div>
-
-                    <div className='rounded-full w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[350px] lg:w-[450px] lg:h-[450px] xl:w-[600px] xl:h-[600px]  bg-light-purple  mb-10 blur-xl'></div>
+                    <div className='rounded-full w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[350px] lg:w-[450px] lg:h-[450px] xl:w-[600px] xl:h-[600px] bg-light-purple mb-10 blur-xl'></div>
                     <div
-                        className='absolute 
-    transition-all duration-500 ease-out 
-    w-full md:w-1/2 h-[320px] md:h-[300px] sm:h-[350px] lg:h-[400px] xl:h-[550px] -top-4 left-16 sm:-top-4  sm:left-17 md:-top-1  md:left-39  lg:-top-4  lg:left-48 xl:-top-5  xl:left-64 '
+                        className='absolute transition-all duration-500 ease-out w-full md:w-1/2 h-[320px] md:h-[300px] sm:h-[350px] lg:h-[400px] xl:h-[550px] -top-4 left-16 sm:-top-4 sm:left-17 md:-top-1 md:left-39 lg:-top-4 lg:left-48 xl:-top-5 xl:left-64'
                     >
                         <Image
                             src='/images/android.png'
                             alt='mobilephone'
                             fill
-                            className='object-contain '
+                            className='object-contain'
                         />
                     </div>
                 </div>
             </div>
 
-            <div className='w-full h-full lg:flex gap-10 flex-col lg:flex-col items-center justify-center my-20 '>
+              <div className="slide2 flex flex-col lg:flex-row justify-center gap-10 items-center">
+                <div className='space-y-6'>
+                    <h2 className='text-3xl font-raleway font-semibold'>
+               Artists, Keep It Simple
+                    </h2>
+                    <p className='md:w-4/5'>
+                      Artists can easily share and update content through a clean, login-based web interface. From uploading new material to keeping fans in the loop with announcements and exclusive content — everything is streamlined to help you focus on creativity and build stronger fan relationships.
+                    </p>
+                </div>
+                <div className='relative flex justify-center items-center md:w-1/2'>
+                    <div
+                        className='absolute transition-all duration-500 ease-out z-10 w-full md:w-1/2 h-[320px] sm:h-[350px] md:h-[300px] lg:h-[400px] xl:h-[550px] bottom-8 -left-12 sm:bottom-12 sm:-left-17 md:bottom-10 md:left-10 lg:bottom-10 lg:left-10 xl:bottom-6 xl:left-12'
+                    >
+                        <Image
+                            src='/images/Stepper2.png'
+                            alt='androidphone'
+                            fill
+                            className='object-contain'
+                        />
+                    </div>
+                    <div className='rounded-full w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[350px] lg:w-[450px] lg:h-[450px] xl:w-[600px] xl:h-[600px] bg-light-purple mb-10 blur-xl'></div>
+                    <div
+                        className='absolute transition-all duration-500 ease-out w-full md:w-1/2 h-[320px] md:h-[300px] sm:h-[350px] lg:h-[400px] xl:h-[550px] -top-4 left-16 sm:-top-4 sm:left-17 md:-top-1 md:left-39 lg:-top-4 lg:left-48 xl:-top-5 xl:left-64'
+                    >
+                        <Image
+                            src='/images/android2.png'
+                            alt='mobilephone'
+                            fill
+                            className='object-contain'
+                        />
+                    </div>
+                </div>
+            </div>
+
+
+              <div className="slide3 flex flex-col lg:flex-row justify-center gap-10 items-center">
+                <div className='space-y-6'>
+                    <h2 className='text-3xl font-raleway font-semibold'>More Than Just a Fan</h2>
+                    <p className='md:w-4/5'>Superfans can register their own accounts to create personalized versions of selected songs, collaborate with other fans, and take part in a vibrant community. They get early access to concert tickets, exclusive events, physical releases, and limited-edition merch — all designed for the most dedicated fans. </p>
+                </div>
+                <div className='relative flex justify-center items-center md:w-1/2'>
+                    <div
+                        className='absolute transition-all duration-500 ease-out z-10 w-full md:w-1/2 h-[320px] sm:h-[350px] md:h-[300px] lg:h-[400px] xl:h-[550px] bottom-8 -left-12 sm:bottom-12 sm:-left-17 md:bottom-10 md:left-10 lg:bottom-10 lg:left-10 xl:bottom-6 xl:left-12'
+                    >
+                        <Image
+                            src='/images/Stepper3.png'
+                            alt='androidphone'
+                            fill
+                            className='object-contain'
+                        />
+                    </div>
+                    <div className='rounded-full w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[350px] lg:w-[450px] lg:h-[450px] xl:w-[600px] xl:h-[600px] bg-light-purple mb-10 blur-xl'></div>
+                    <div
+                        className='absolute transition-all duration-500 ease-out w-full md:w-1/2 h-[320px] md:h-[300px] sm:h-[350px] lg:h-[400px] xl:h-[550px] -top-4 left-16 sm:-top-4 sm:left-17 md:-top-1 md:left-39 lg:-top-4 lg:left-48 xl:-top-5 xl:left-64'
+                    >
+                        <Image
+                            src='/images/android3.png'
+                            alt='mobilephone'
+                            fill
+                            className='object-contain'
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className='w-full h-full lg:flex gap-10 flex-col items-center justify-center my-20'>
                 <h1 className='text-4xl lg:text-5xl p-2 font-semibold font-raleway lg:w-2/3'>
-                    Head to our registration page and launch your own Superfan
-                    app today!
+                    Head to our registration page and launch your own Superfan app today!
                 </h1>
                 <div className='w-full flex justify-center max-w-3xl my-3'>
-                    <Button
-                        className='rounded-sm px-16 w-full sm:w-auto'
-                        variant='primary'
-                    >
+                    <Button className='rounded-sm px-16 w-full sm:w-auto' variant='primary'>
                         Sign Up
                     </Button>
                 </div>
