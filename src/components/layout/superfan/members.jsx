fca5473b2c42920578c663e9b4fa95721b2a8f43
Monika <maam092024@gmail.com>
2025-07-04 14:51:44 +0200

    members pic quality update

--- src/components/layout/superfan/members.jsx
+++ src/components/layout/superfan/members.jsx
@@ -2,32 +2,32 @@ export const members = [
   {
     name: "Lena",
     opinion: "Loved the experience! The artist truly connected with us.",
-    src: "/members/Rectangle 19.png",
+    src: "/members/Rectangle 19.jpg",
   },
   {
     name: "Kaya",
     opinion: "It felt so personal and powerful. Definitely coming back!",
-    src: "/members/Rectangle 20.png",
+    src: "/members/Rectangle 20.jpg",
   },
   {
     name: "Nora",
     opinion:
       "A beautiful moment shared with the community and the my favourite band...",
-    src: "/members/Rectangle 21.png",
+    src: "/members/Rectangle 21.jpg",
   },
   {
     name: "Elise",
     opinion: "I felt seen and heard. Thank you for making this happen!",
-    src: "/members/Rectangle 22.png",
+    src: "/members/Rectangle 22.jpg",
   },
   {
     name: "Milo",
     opinion: "Such a unique way to interact — pure magic!",
-    src: "/members/Rectangle 23.png",
+    src: "/members/Rectangle 23.jpg",
   },
   {
     name: "Ravi",
     opinion: "Never experienced anything like this before. Wow! ",
-    src: "/members/Rectangle 24.png",
+    src: "/members/Rectangle 24.jpg",
   },
 ];
